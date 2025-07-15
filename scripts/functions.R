9a75e332e6c54e0f6ad7e2e170b8860102d71caf
SShekarriz <shahrokh.shekarriz@gmail.com>
2025-07-15 15:08:05 -0400

    add integration and visualization in wrapper funcs

--- scripts/functions.R
+++ scripts/functions.R
@@ -87,21 +87,82 @@ countColoniz <- function(donor_feature,
 }
 
 # get total coloniation count- wraper for two above functions
-wrapColoniz <- function(feature_c = "../data/clean/binCover.txt", 
-                        donor = "DonA",
+CountFig <- function(feature_c = "../data/clean/binCover.txt", 
                         cutoff_pres = 0,
                         method = "Assembly",
                         mapfile = "../data/clean/mapfile.txt") {
   
-  donor_feature <- DonFeature(feature_c = feature_c, donor = donor,
-             cutoff_pres = cutoff_pres, method = method)
+  # DonorA
+  donorA_feature <- DonFeature(feature_c = feature_c, donor = "DonA",
+                              cutoff_pres = cutoff_pres, method = method)
+  finalA_count <- countColoniz(donor_feature = donorA_feature, 
+                              cutoff_pres = cutoff_pres,
+                              mapfile = mapfile) %>%
+                  mutate(Donor = paste("DonorA"))
+  # DonorB
+  donorB_feature <- DonFeature(feature_c = feature_c, donor = "DonB",
+                               cutoff_pres = cutoff_pres, method = method)
+  finalB_count <- countColoniz(donor_feature = donorB_feature, 
+                               cutoff_pres = cutoff_pres,
+                               mapfile = mapfile) %>%
+                  mutate(Donor = paste("DonorB"))
   
-  final_count <- countColoniz(donor_feature = donor_feature, 
-               cutoff_pres = cutoff_pres,
-               mapfile = mapfile)
+  countfig <- finalA_count%>%
+    rbind(finalB_count) %>%
+    ggplot(
+      aes(Group, n_coloniz)) +
+    geom_boxplot() + geom_point() +
+    facet_grid(Donor~Category, space = "free", scales = "free_x")
   
-  return(final_count)
+  return(countfig)
 }
 
+# a function to compare the colonized feature in pups
+identColoniz <- function(donor_feature,
+                         cutoff_pres = 0,
+                         mapfile = "../data/clean/mapfile.txt"){
+  
+  # Read mapfile
+  map <- read.delim(mapfile, header = TRUE, 
+                    stringsAsFactors = FALSE)
+  
+  coloniz_ident = donor_feature %>%
+    filter(coverage >= cutoff_pres) %>%
+    left_join(map %>% select(Sample, Group, Category)) %>%
+    filter(Category == "Pup") %>% select(-Category)
+  
+  return(coloniz_ident)
+}
 
+# compare colonized feature in pups- wrapper for two above functions
+CompFig <- function(feature_c = "../data/clean/binCover.txt", 
+                        cutoff_pres = 0,
+                        method = "Assembly",
+                        mapfile = "../data/clean/mapfile.txt") {
+  
+  # DonorA
+  donorA_feature <- DonFeature(feature_c = feature_c, donor = "DonA",
+                               cutoff_pres = cutoff_pres, method = method)
+  finalA_identity <- identColoniz(donor_feature = donorA_feature, 
+                               cutoff_pres = cutoff_pres,
+                               mapfile = mapfile) %>%
+                    mutate(Donor = paste("DonorA"))
+  
+  # DonorB
+  donorB_feature <- DonFeature(feature_c = feature_c, donor = "DonB",
+                               cutoff_pres = cutoff_pres, method = method)
+  finalB_identity <- identColoniz(donor_feature = donorB_feature, 
+                                  cutoff_pres = cutoff_pres,
+                                  mapfile = mapfile) %>%
+                    mutate(Donor = paste("DonorB"))
+  
+  compfig <- finalA_identity%>%
+    rbind(finalB_identity) %>%
+    ggplot(
+      aes(feature, Sample, fill= coverage)) +
+    geom_tile() +
+    facet_grid(Group~Donor, space = "free", scales = "free")
+  
+  return(compfig)
+}
 
